2eedc2e9d934e8d68561f3425bf8a6aba14b4d2f
arkandria <miguelfariasgp@gmail.com>
2022-07-12 16:56:41 -0500

    finish exercise, test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+  let newString = "";
+  if (num<0) {
+      return "ERROR";
+  }else{
+  for (i=0; i<=num-1; i++) {
+  
+  newString = newString + string;
+}
+return newString;
+}
 };
 
 // Do not edit below this line
