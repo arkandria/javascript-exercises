b3e527a79b22c5d96fbdcf1c743d9d52866fa6ce
arkandria <miguelfariasgp@gmail.com>
2022-07-13 09:17:29 -0500

    finish exercise, test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray=string.split("");
+    let reversedArray= stringArray.reverse();
+    let revString= reversedArray.join('');
+    return revString;
 };
 
 // Do not edit below this line
