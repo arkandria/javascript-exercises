d6bfb7bda133e25774b51d0b6a28c6d124500f9c
arkandria <miguelfariasgp@gmail.com>
2022-07-13 12:46:10 -0500

    finish exercise, test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let cels= (f-32)*(5/9);
+  if (Number.isInteger(cels)) {
+    return cels;
+  } else {
+    return Number((Math.round(cels * 10) / 10).toFixed(1));
+  }
+  
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let far= (c*(9/5) ) +32;
+  if (Number.isInteger(far)) {
+    return far;
+  } else {
+    return Number((Math.round(far * 10) / 10).toFixed(1));
+  }
 };
 
 // Do not edit below this line
