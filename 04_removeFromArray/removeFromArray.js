1b9ed8d72caba6cd284e2e1c3b88363b80bd126a
arkandria <miguelfariasgp@gmail.com>
2022-07-13 10:42:22 -0500

    finish exercise, test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...others) {
+    let newArray = array;
+    
+    let argumentArray = others;
+    
+    for (let i=0; i<argumentArray.length; i++) {
+        for (let j=0; j<newArray.length; j++) {
+            
+            
+            if (newArray[j]===argumentArray[i]){
+                newArray.splice(j,1);
+            }
+        }
+    }
+    
+    return newArray;
 };
 
 // Do not edit below this line
