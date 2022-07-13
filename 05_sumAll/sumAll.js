783543191bb1460a0bc568cb466d7e7ebe4f3657
arkandria <miguelfariasgp@gmail.com>
2022-07-13 11:39:51 -0500

    finish exercise, test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(firstN,lastN) {
+    if ( typeof firstN !== 'number' || typeof lastN !== 'number' || firstN < 0 || lastN < 0 ) {
+        return "ERROR";
+    } else  if (firstN<lastN){
+        let a=firstN;
+        let b=lastN;
+        let total=0;
+    
+    for (i=a; i<=b; i++) {
+        total= total+i;
+        
+    }
+    
+    return total;
+    } else {
+        let b=firstN;
+        let a=lastN;
+        let total=0;
+    
+    for (i=a; i<=b; i++) {
+        total= total+i;
+        
+    }
+    
+    return total;
+    }
+    
 
 };
 
