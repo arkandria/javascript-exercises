9e4d58f94577ab0892bd32c161736d86e99a53f2
arkandria <miguelfariasgp@gmail.com>
2022-07-21 13:16:54 -0500

    complete exercise passed tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+
+
+const findTheOldest = function(arr) {
+    arr.forEach( (el) =>{
+        if (el.hasOwnProperty('yearOfDeath')) {
+            el.age = el.yearOfDeath - el.yearOfBirth;
+            console.log (el.age);
+            
+            
+        } else {
+            el.age = new Date().getFullYear() - el.yearOfBirth
+            console.log(el.age);
+        }
+    })
+    arr.sort (function(a, b){
+        return b.age -a.age});
+    
+    arr.forEach( (el) =>{
+        delete el.age;
+    });
+
+    return arr[0];
 
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
