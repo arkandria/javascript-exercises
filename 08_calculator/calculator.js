d2bac9f187117d2a32f68469ec797ece3f30269b
arkandria <miguelfariasgp@gmail.com>
2022-07-21 09:02:11 -0500

    complete exercise passed tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+    arr.forEach( function (e) {
+        total += e;
+    });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  arr.forEach( (e) => total *= e);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x,n) {
+	let total = x;
+  for (i=1; i<n; i++) { 
+    total *= x
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let arr = [];
+  let total = 1;
+  for (i=1; i<=n; i++) {
+    arr.push(i);
+  };
+  arr.forEach( (e) => total *= e);
+  return total;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
