4987285755b7026ccef50928bf9d8b96dd728b6d
arkandria <miguelfariasgp@gmail.com>
2022-07-21 11:28:28 -0500

    complete exercise passed tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n>0){
+        let arr = [1,1];
+        for (i=0; i<=n; i++) {
+            let x = arr[i] + arr[i+1];
+            arr.push(x);
+        }
+        console.log (arr);
+        return arr[n-1];
+    } else {
+        return "OOPS"
+    }
 };
 
+console.log(fibonacci(15));
 // Do not edit below this line
 module.exports = fibonacci;
